5317a572fead4ae355afaa281b528dff4fb34533
benzyx <frenzybenzy@gmail.com>
2020-04-16 02:10:57 -0400

    italian column renaming

--- R/ThompsonModel.R
+++ R/ThompsonModel.R
@@ -7,22 +7,18 @@ library(ggplot2)
 #' Let's use the Italian Provincial datasets.
 #'
 dpc_df <- read.csv("data/dpc-covid19-ita-province.csv")
-region_names <- unique(dpc_df$denominazione_provincia)
+province_names <- unique(dpc_df$province)
 all_res_parametric_si <- data.frame()
 
 
 augmented_df <- read.csv("data/dpc-augmented.csv")
-variables_df <- subset(augmented_df, select=c("denominazione_provincia", "gdppercapita", "density", "date", "air_temp", "dew_point"))
+variables_df <- subset(augmented_df, select=c("province", "gdppercapita", "density", "date", "air_temp", "dew_point"))
 
-
-
-for (region in region_names) {
-  region_df <- subset(dpc_df, denominazione_provincia == region, select = c("data", "totale_casi"))
-  names(region_df)[1] <- "date"
-  names(region_df)[2] <- "cases"
+for (province_name in province_names) {
+  region_df <- subset(dpc_df, province == province_name, select = c("date", "total_cases"))
   
   #' Add a "new cases" column for per-day data.
-  region_df <- within(region_df, new_cases <- c(0, diff(cases)))
+  region_df <- within(region_df, new_cases <- c(0, diff(total_cases)))
   region_df$new_cases[region_df$new_cases<0] <- 0
   region_df
   
