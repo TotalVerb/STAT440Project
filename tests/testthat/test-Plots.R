c951b206bec7d60b931146dd48d2fb784b8fb271
Fengyang Wang <fengyang.wang.0@gmail.com>
2020-04-17 19:28:20 -0400

    another plots test

--- tests/testthat/test-Plots.R
+++ tests/testthat/test-Plots.R
@@ -5,3 +5,12 @@ test_that("80% confidence interval works", {
   expect_equal(results$ymin, 10.9)
   expect_equal(results$ymax, 90.1)
 })
+
+test_that("remove year of date range works on various types", {
+  range <- c("2019-07-02", "2019-07-03", "2030-01-01")
+  ry1 <- removeyear(range)
+  ry2 <- removeyear(as.POSIXct(range))
+  expect_equal(ry1, c("07-02", "07-03", "01-01"))
+  expect_equal(ry2, c("07-02", "07-03", "01-01"))
+})
+
